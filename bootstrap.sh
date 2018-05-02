8f45404574d00a607efe9fd1841634a67233548b
Erik Boström <erikbos@mech.kth.se>
2018-05-02 12:19:55 +0200

    Color prompt edited

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 
 # list of files/folders to symlink in homedir
-files=".bashrc .bash_exports .bash_profiles .bash_function . bash_aliases .emacs.d"
+files=".bashrc .bash_exports .bash_profiles .bash_function .bash_aliases .emacs.d"
 
 
 ##########
